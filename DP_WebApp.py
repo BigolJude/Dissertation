441db6f12ab4bc0a35206f0de41774eed21bae5f
BigolJude <judebarnett2018@gmail.com>
2024-04-18 13:08:21 +0100

    Mapping working

--- DP_WebApp.py
+++ DP_WebApp.py
@@ -5,6 +5,13 @@ from dash_extensions.javascript import assign
 
 app = Dash(name=__name__, external_stylesheets=[DashBootstrap.themes.SLATE])
 
+style_handle = assign("""function(feature, context){
+    const {selected} = context.hideout;
+    if(selected.includes(feature.properties.name)){
+        return {fillColor: 'red', color: 'grey'}
+    }
+    return {fillColor: 'grey', color: 'grey'}
+}""")
 
 app.layout = html.Div([
     html.Div([
@@ -12,18 +19,18 @@ app.layout = html.Div([
             html.H1(children='Development Project'),
             html.H2(children='Mapped Financial Risk by Area'),
             DashLeaflet.Map([
-    DashLeaflet.TileLayer(),
-    DashLeaflet.GeoJSON(url="datasets/RegionsUK.geojson", zoomToBounds=True, id="geojson",
-               hideout=dict(selected=[]), style=style_handle)
-], style={'height': '50vh'}, center=[56, 10], zoom=6)
+                DashLeaflet.TileLayer(),
+                DashLeaflet.GeoJSON(url="/assets/eer.json", zoomToBounds=True, id="geojson",
+                                   hideout=dict(selected=[]), style=style_handle)
+                ], style={'height': '50vh'}, center=[56, 10], zoom=6)
         ],style={
             'display': 'flex',
-            'flex-direction': 'column',
-            'text-align': 'center'})
+            'flexDirection': 'column',
+            'textAlign': 'center'})
     ], style={
         'display': 'flex',
-        'justify-content':'center',
-        'align-content':'centre'})
+        'justifyContent':'center',
+        'alignContent':'centre'})
 ])
 
 if __name__ == '__main__':
